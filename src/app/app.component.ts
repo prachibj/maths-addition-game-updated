990621d8a953f2b30196e90bee5d28fd3dada500
Prachi <prachibhave14@gmail.com>
2023-02-22 20:04:58 +0530

    average time added for correct answers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,15 @@ export class AppComponent {
   time: any;
   bootstrapModal: any;
   isReset: boolean = false;
+  averageTime: any;
+  avg: any = 0;
+  counter: number = 0;
 
-  constructor(private interaction: InteractionService) {}
+  constructor(private interaction: InteractionService) { }
 
   ngOnInit() {
+    this.averageTime=localStorage.getItem('averageTime')
+
     this.startTime = new Date().getTime()
     this.interaction.getData().subscribe((res: any) => {
       console.log(res);
@@ -38,10 +43,21 @@ export class AppComponent {
     if (this.answer === this.num1 + this.num2) {
       this.isCorrect = true;
       this.isIncorrect = false;
+      this.counter++;
       this.time = Math.round((this.endTime - this.startTime) / 1000);
+      this.avg += this.time
+      this.averageTime = this.avg / this.counter;
+      if (localStorage.getItem('averageTime')) {
+        let x: any = localStorage.getItem('averageTime');
+        this.averageTime = (this.averageTime + parseInt(x)) / 2
+      }
+      console.log(this.averageTime, this.counter, this.avg, this.time);
+      localStorage.setItem('averageTime', this.averageTime)
       this.showResultModal();
       this.reset();
     } else {
+      this.averageTime = 0;
+      localStorage.setItem('averageTime', this.averageTime)
       this.isCorrect = false;
       this.isIncorrect = true;
       this.showResultModal()
