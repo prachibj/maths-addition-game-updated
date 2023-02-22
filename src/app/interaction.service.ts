51f879e26bd4ecb31be87f7447544d3b426d75a5
Prachi <prachibhave14@gmail.com>
2023-02-22 17:46:59 +0530

    remove logs

--- src/app/interaction.service.ts
+++ src/app/interaction.service.ts
@@ -1,24 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InteractionService {
 
-  constructor() { }
-
-  private subject = new Subject<any>();
-
-
-  private randomNumber$ = new Observable(observer => {  
+  private randomNumber$ = new Observable(observer => {
     const num1 = Math.floor(Math.random() * 9) + 1;
     const num2 = Math.floor(Math.random() * 9) + 1;
-    observer.next({num1:num1,num2:num2});  
-  }); 
+    observer.next({ num1: num1, num2: num2 });
+  });
 
   getData() {
-    console.log(this.subject)
     return this.randomNumber$;
   }
 
